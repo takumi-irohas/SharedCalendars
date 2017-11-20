0fd002ad080082471a6ec27dc98fc69e5dd64b1b
masamasa <m.higashi.8912@gmail.com>
2017-11-20 22:35:35 +0900

    非同期通信の改良

--- app/src/main/java/higa/sharedcalendars/view/activity/CalendarActivity.java
+++ app/src/main/java/higa/sharedcalendars/view/activity/CalendarActivity.java
@@ -19,6 +19,8 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -62,7 +64,7 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 	static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
 	static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
 
-	private static final String BUTTON_TEXT = "Call Google Calendar API";
+	private static final String BUTTON_TEXT = "今後の予定";
 	private static final String PREF_ACCOUNT_NAME = "accountName";
 	private static final String[] SCOPES = { CalendarScopes.CALENDAR_READONLY };
 
@@ -90,15 +92,19 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		mOutputText.setVerticalScrollBarEnabled(true);
 		mOutputText.setMovementMethod(new ScrollingMovementMethod());
 		mOutputText.setText(
-				"Click the \'" + BUTTON_TEXT +"\' button to test the API.");
+				"ボタンをクリックすると今後10日間予定されている予定が出力されます。");
 
 		mProgress = new ProgressDialog(this);
-		mProgress.setMessage("Calling Google Calendar API ...");
+		mProgress.setMessage("Calling My Google Calendar ...");
 
 		// Initialize credentials and service object.
 		mCredential = GoogleAccountCredential.usingOAuth2(
 				getApplicationContext(), Arrays.asList(SCOPES))
 				.setBackOff(new ExponentialBackOff());
+
+		WebView webView = (WebView)findViewById(R.id.web_view);
+		webView.setWebViewClient(new WebViewClient());
+		webView.loadUrl("https://google.com/");
 	}
 
 	@Override
@@ -113,18 +119,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		return true;
 	}
 
-	/**
-	 * Create the main activity.
-	 * @param savedInstanceState previously saved instance data.
-	 */
-
-	/**
-	 * Attempt to call the API, after verifying that all the preconditions are
-	 * satisfied. The preconditions are: Google Play Services installed, an
-	 * account was selected and the device currently has online access. If any
-	 * of the preconditions are not satisfied, the app will prompt the user as
-	 * appropriate.
-	 */
 	private void getResultsFromApi() {
 		if (! isGooglePlayServicesAvailable()) {
 			acquireGooglePlayServices();
@@ -137,16 +131,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		}
 	}
 
-	/**
-	 * Attempts to set the account used with the API credentials. If an account
-	 * name was previously saved it will use that one; otherwise an account
-	 * picker dialog will be shown to the user. Note that the setting the
-	 * account to use with the credentials object requires the app to have the
-	 * GET_ACCOUNTS permission, which is requested here if it is not already
-	 * present. The AfterPermissionGranted annotation indicates that this
-	 * function will be rerun automatically whenever the GET_ACCOUNTS permission
-	 * is granted.
-	 */
 	@AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
 	private void chooseAccount() {
 		if (EasyPermissions.hasPermissions(
@@ -172,16 +156,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		}
 	}
 
-	/**
-	 * Called when an activity launched here (specifically, AccountPicker
-	 * and authorization) exits, giving you the requestCode you started it with,
-	 * the resultCode it returned, and any additional data from it.
-	 * @param requestCode code indicating which activity result is incoming.
-	 * @param resultCode code indicating the result of the incoming
-	 *     activity result.
-	 * @param data Intent (containing result data) returned by incoming
-	 *     activity result.
-	 */
 	@Override
 	protected void onActivityResult(
 			int requestCode, int resultCode, Intent data) {
@@ -220,14 +194,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		}
 	}
 
-	/**
-	 * Respond to requests for permissions at runtime for API 23 and above.
-	 * @param requestCode The request code passed in
-	 *     requestPermissions(android.app.Activity, String, int, String[])
-	 * @param permissions The requested permissions. Never null.
-	 * @param grantResults The grant results for the corresponding permissions
-	 *     which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
-	 */
 	@Override
 	public void onRequestPermissionsResult(int requestCode,
 																				 @NonNull String[] permissions,
@@ -237,34 +203,16 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 				requestCode, permissions, grantResults, this);
 	}
 
-	/**
-	 * Callback for when a permission is granted using the EasyPermissions
-	 * library.
-	 * @param requestCode The request code associated with the requested
-	 *         permission
-	 * @param list The requested permission list. Never null.
-	 */
 	@Override
 	public void onPermissionsGranted(int requestCode, List<String> list) {
 		// Do nothing.
 	}
 
-	/**
-	 * Callback for when a permission is denied using the EasyPermissions
-	 * library.
-	 * @param requestCode The request code associated with the requested
-	 *         permission
-	 * @param list The requested permission list. Never null.
-	 */
 	@Override
 	public void onPermissionsDenied(int requestCode, List<String> list) {
 		// Do nothing.
 	}
 
-	/**
-	 * Checks whether the device currently has a network connection.
-	 * @return true if the device has a network connection, false otherwise.
-	 */
 	private boolean isDeviceOnline() {
 		ConnectivityManager connMgr =
 				(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -272,11 +220,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		return (networkInfo != null && networkInfo.isConnected());
 	}
 
-	/**
-	 * Check that Google Play services APK is installed and up to date.
-	 * @return true if Google Play Services is available and up to
-	 *     date on this device; false otherwise.
-	 */
 	private boolean isGooglePlayServicesAvailable() {
 		GoogleApiAvailability apiAvailability =
 				GoogleApiAvailability.getInstance();
@@ -285,10 +228,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		return connectionStatusCode == ConnectionResult.SUCCESS;
 	}
 
-	/**
-	 * Attempt to resolve a missing, out-of-date, invalid or disabled Google
-	 * Play Services installation via a user dialog, if possible.
-	 */
 	private void acquireGooglePlayServices() {
 		GoogleApiAvailability apiAvailability =
 				GoogleApiAvailability.getInstance();
@@ -299,13 +238,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		}
 	}
 
-
-	/**
-	 * Display an error dialog showing that Google Play Services is missing
-	 * or out of date.
-	 * @param connectionStatusCode code describing the presence (or lack of)
-	 *     Google Play Services on this device.
-	 */
 	void showGooglePlayServicesAvailabilityErrorDialog(
 			final int connectionStatusCode) {
 		GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
@@ -316,10 +248,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 		dialog.show();
 	}
 
-	/**
-	 * An asynchronous task that handles the Google Calendar API call.
-	 * Placing the API calls in their own task ensures the UI stays responsive.
-	 */
 	private class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
 		private com.google.api.services.calendar.Calendar mService = null;
 		private Exception mLastError = null;
@@ -333,10 +261,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 					.build();
 		}
 
-		/**
-		 * Background task to call Google Calendar API.
-		 * @param params no parameters needed for this task.
-		 */
 		@Override
 		protected List<String> doInBackground(Void... params) {
 			try {
@@ -348,11 +272,6 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 			}
 		}
 
-		/**
-		 * Fetch a list of the next 10 events from the primary calendar.
-		 * @return List of Strings describing returned events.
-		 * @throws IOException
-		 */
 		private List<String> getDataFromApi() throws IOException {
 			// List the next 10 events from the primary calendar.
 			DateTime now = new DateTime(System.currentTimeMillis());
@@ -390,7 +309,7 @@ public class CalendarActivity extends AppCompatActivity implements EasyPermissio
 			if (output == null || output.size() == 0) {
 				mOutputText.setText("No results returned.");
 			} else {
-				output.add(0, "Data retrieved using the Google Calendar API:");
+				output.add(0, "今後10日間の予定:");
 				mOutputText.setText(TextUtils.join("\n", output));
 			}
 		}
